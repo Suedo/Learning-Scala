9472ee1cdf3fc87a6e447785d2dffdf8c22ffc24
snag <somjitnag192@gmail.com>
2020-03-15 15:09:26 +0530

    adding toString and Sorting

--- FP/src/main/scala/Scala4Java/InheritanceDemo.scala
+++ FP/src/main/scala/Scala4Java/InheritanceDemo.scala
@@ -1,8 +1,12 @@
 package Scala4Java
 
+import scala.util.Sorting
+
 object test extends App {
 
-  class Item(val name: String, val price: Double)
+  class Item(val name: String, val price: Double){
+    override def toString: String = s"${name}"
+  }
   object Item {
     def apply(name: String, price: Double): Item = new Item(name, price)
   }
@@ -13,7 +17,7 @@ object test extends App {
   }
 
   class Customer(val name: String, val address: Address) {
-    var cart: Map[Item, Int] = Map(Item("", 0) -> 0)
+    var cart: Map[Item, Int] = Map()
     private val discount: Int = 0
 
     def addToCart(item: Item, count: Int) = {
@@ -30,6 +34,12 @@ object test extends App {
         startingAmount + (cartItem._2 * priceOfItem)
       })
     }
+
+    override def toString: String = s"${name}'s cart: ${cart}. Total: ${total()}"
+  }
+
+  object CartComparator extends Ordering[Customer] {
+    def compare(a: Customer, b:Customer) = (a.total() - b.total()).toInt
   }
 
   class DiscountedCustomer(name: String, address: Address, val discount: Int) extends Customer(name, address) {
@@ -37,10 +47,23 @@ object test extends App {
   }
 
   val home: Address = Address("18th Cross", "Bangalore", 560102)
-  val cust = new DiscountedCustomer("Somjit", home, 5)
+  val cust1 = new DiscountedCustomer("Somjit", home, 5)
+  val cust2 = new DiscountedCustomer("CC", home, 10)
+  val cust3 = new DiscountedCustomer("Sanji", home, 20)
 
-  cust.addToCart(Item("cabbage", 20), 1)
+
+
+  cust1.addToCart(Item("cabbage", 20), 1)
     .addToCart(Item("pepsi", 80), 1)
 
-  println("Total Expense: " + cust.total()) // 95
+
+  cust2.addToCart(Item("lotion", 120), 1)
+    .addToCart(Item("cream", 80), 1)
+
+
+  cust3.addToCart(Item("suit", 1200), 1)
+    .addToCart(Item("mushrooms", 80), 1)
+
+  val custList = List(cust1, cust2, cust3)
+  for (cust <- custList.sorted(CartComparator)) println(cust)
 }
